Added functionalities for gradient verification (computation with PyToch AD and finite differences) Fixed noisy first guess runs. Added a preliminary file for all visualization related methods (work in progress).

cobbi/core/data_manipulation.py
@@ -1,11 +1,12 @@
 import numpy as np
 import rasterio
 import shutil
+import salem
 from noise import pnoise2
 from scipy.ndimage import interpolation
 
 
-def add_noise_to_first_guess(gdir, noise):
+def add_noise_to_first_guess(gdir, noise, cut_noise=True, min_ice_thick=5):
     """
     Adds noise to the first guess. Saves this to the file with the first guess
     and also saves the applied noise.
@@ -16,8 +17,12 @@ def add_noise_to_first_guess(gdir, noise):
         GlacierDirectory containing the first guess
     noise: ndarray
         noise to apply to the first guess
+    cut_noise: bool
+        whether or not the noise should be cut to not penetrate the surface
+        and require a minimum ice thickness if applied to first guess
+    min_ice_thick: float
+        minimum ice thickness, only applied if noise is cut
     """
-    np.save(gdir.get_filepath('first_guessed_bed_noise'), noise)
 
     fg_filepath = gdir.get_filepath('first_guessed_bed')
 
@@ -25,11 +30,30 @@ def add_noise_to_first_guess(gdir, noise):
         first_guessed_bed = src.read(1)
         profile = src.profile
 
+    if cut_noise:
+        desired_rmse = np.sqrt(np.mean(noise ** 2))
+        ref_ice_mask = np.load(gdir.get_filepath('ref_ice_mask'))
+        ref_surf = salem.GeoTiff(gdir.get_filepath('ref_dem')).get_vardata()
+
+        penetrating = (first_guessed_bed + noise - min_ice_thick > ref_surf)
+        penetrating *= ref_ice_mask
+
+        noise = np.where(penetrating,
+                         ref_surf - first_guessed_bed - min_ice_thick,
+                         noise)
+        print('desired rmse: {:g}\\rmse after cutting: {:g}'.format(
+            desired_rmse, np.sqrt(np.mean(noise ** 2))))
+        # TODO: will result in problems
+        # rmse = np.sqrt(np.mean(noise ** 2))
+        # noise *= desired_rmse / rmse  # rescale to desired RMSE
+
     first_guessed_bed = first_guessed_bed + noise
 
     profile['dtype'] = 'float64'
     with rasterio.open(fg_filepath, 'w', **profile) as dst:
         dst.write(first_guessed_bed, 1)
+
+    np.save(gdir.get_filepath('first_guessed_bed_noise'), noise)
 
 
 def take_true_bed_as_first_guess(gdir):
@@ -117,6 +141,22 @@ def create_noise(gdir, std=3, zoom=-1, glacier_only=True):
 
 def create_perlin_noise(gdir, desired_rmse=5., octaves=1, base=1., freq=8.0,
                         glacier_only=True):
+    """
+    TODO: Documentation
+
+    Parameters
+    ----------
+    gdir
+    desired_rmse
+    octaves
+    base
+    freq
+    glacier_only
+
+    Returns
+    -------
+
+    """
 
     ref_ice_mask = np.load(gdir.get_filepath('ref_ice_mask'))
     max_y, max_x = ref_ice_mask.shape
@@ -128,10 +168,10 @@ def create_perlin_noise(gdir, desired_rmse=5., octaves=1, base=1., freq=8.0,
             noise[y, x] = pnoise2(x / freq, y / freq, octaves=octaves,
                                   base=base)
 
-    rmse = np.sqrt(np.mean(noise**2))
-    noise *= desired_rmse / rmse
-
     if glacier_only:
         noise = noise * ref_ice_mask
+
+    rmse = np.sqrt(np.mean(noise**2))
+    noise *= desired_rmse / rmse
 
     return noise

cobbi/core/test_cases.py
@@ -218,9 +218,9 @@ Owen.mb_grad = 3.5
 Owen.smooth_border_px = 2
 Owen.smooth_border_h = 300
 
-#Nanisivik / Arctic Bay
+#Borden Peninsula
 Nanisivik = TestCase()
-Nanisivik.name = 'Nanisivik Arctic Bay'
+Nanisivik.name = 'Borden Peninsula'
 Nanisivik.extent= np.array([[-85.88, 72.34], [-83.40, 73.07]])
 Nanisivik.ela_h = 625
 Nanisivik.dx = 2000

cobbi/sandbox/noisy_first_guess_inversion_run.py
@@ -16,24 +16,22 @@ from cobbi.core.data_manipulation import take_true_bed_as_first_guess
 from cobbi.core.data_manipulation import add_noise_to_first_guess
 from cobbi.core.data_manipulation import create_perlin_noise
 from oggm import cfg
+import itertools
 
 cfg.initialize()
 
-desired_rmse = 5
-freq = 8.  # determines coherence of noise. Larger frequ gives larger
-# coherence
-base = 3  # works like np.random.seed
+first_run = False  # set to true to (re)run spinup and creation of glacier
 
 basedir = '/path/to/example'
 basedir = '/data/philipp/thesis_test2/Giluwe/noisy_first_guess/'
-
 
 # TODO: think about IceThicknesses for case Giluwe
 # Choose a case
 case = test_cases.Giluwe
 gdir = NonRGIGlacierDirectory(case, basedir)
 # only needed once:
-#gis.define_nonrgi_glacier_region(gdir)
+if first_run:
+    gis.define_nonrgi_glacier_region(gdir)
 
 # create settings for inversion
 lambdas = np.zeros(4)
@@ -54,48 +52,69 @@ minimize_options = {
     'disp': True
 }
 
-subdir = 'rmse{:g}_freq{:g}_base_{:d}/'.format(desired_rmse, freq, base)
+# ------ set parameters for noise creation
+octaves = 4  # found to give good results
+desired_rmses = [5, 10, 20, 30, 50]
+freqs = [12, 8, 6, 4, 3] # determines coherence of noise. Larger freq gives
+# larger
+# coherence
+bases = [0, 1, 2, 3, 4]  # works like np.random.seed
+all_noise_parameters = itertools.product(desired_rmses, freqs, bases)
+# print(list(all_noise_parameters))
 
-# Theoretically also only needed once
-gdir.write_inversion_settings(mb_spinup=None,
-                              yrs_spinup=2000,
-                              yrs_forward_run=200,
-                              reg_parameters=lambdas,
-                              solver='L-BFGS-B',
-                              minimize_options=minimize_options,
-                              inversion_subdir=subdir,
-                              fg_shape_factor=1.,
-                              bounds_min_max=(2, 600)
-                              )
+for noise_parameters in all_noise_parameters:
+    # TODO: allow for parallelization of this for loop.
+    # Therefore, noise, inversion settings, ... have to be created in the
+    # idir instead of gdir from beginning
+    subdir = 'noise_{:02d}_{:02d}_{:d}/'.format(noise_parameters[0],
+                                                noise_parameters[1],
+                                                noise_parameters[2])
 
-# Optional, if not reset=True and already ran once
-# only needed once:
-#create_glacier(gdir)
-#     compile_first_guess(gdir)
+    # Theoretically also only needed once
+    gdir.write_inversion_settings(mb_spinup=None,
+                                  yrs_spinup=2000,
+                                  yrs_forward_run=200,
+                                  reg_parameters=lambdas,
+                                  solver='L-BFGS-B',
+                                  minimize_options=minimize_options,
+                                  inversion_subdir=subdir,
+                                  fg_shape_factor=1.,
+                                  bounds_min_max=(2, 600)
+                                  )
 
-noise = create_perlin_noise(gdir, desired_rmse, base=base, freq=freq,
-                            glacier_only=True)
-plt.figure()
-plt.imshow(noise)
-plt.show()
+    # Optional, if not reset=True and already ran once
+    # only needed once:
+    if first_run:
+        create_glacier(gdir)
+        first_run = False
+    # compile_first_guess(gdir)
 
-take_true_bed_as_first_guess(gdir)
-add_noise_to_first_guess(gdir, noise)
+    noise = create_perlin_noise(gdir, noise_parameters[0],
+                                octaves=octaves,
+                                base=noise_parameters[2],
+                                freq=noise_parameters[1],
+                                glacier_only=True)
+    #plt.figure()
+    #plt.imshow(noise)
+    #plt.show()
 
-idir = InversionDirectory(gdir)
+    take_true_bed_as_first_guess(gdir)
+    add_noise_to_first_guess(gdir, noise)
 
-# copy this script to inversion directory for reproducibility
-path_to_file = '/home/philipp/COBBI/cobbi/sandbox' \
-               '/noisy_first_guess_inversion_run.py'
-fname = os.path.split(path_to_file)[-1]
-dst_dir = idir.get_current_basedir()
-if not os.path.exists(dst_dir):
-    os.makedirs(dst_dir, exist_ok=True)
-shutil.copy(path_to_file, os.path.join(dst_dir, fname))
+    idir = InversionDirectory(gdir)
 
-# Finally run inversion
-res = idir.run_minimize()
-#dl = data_logging.load_pickle(idir.get_current_basedir() + '/data_logger.pkl')
+    # copy this script to inversion directory for reproducibility
+    path_to_file = '/home/philipp/COBBI/cobbi/sandbox' \
+                   '/noisy_first_guess_inversion_run.py'
+    fname = os.path.split(path_to_file)[-1]
+    dst_dir = idir.get_current_basedir()
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir, exist_ok=True)
+    shutil.copy(path_to_file, os.path.join(dst_dir, fname))
+
+    # Finally run inversion
+    res = idir.run_minimize()
+    #dl = data_logging.load_pickle(idir.get_current_basedir() + '/data_logger.pkl')
 
 
 

cobbi/temp/noise_test.py
@@ -3,19 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-octaves = 1
+octaves = 4
 freq = 8. * octaves
 max_y = 50
 max_x = 40
 
-for base in [0, 1, 2]:
-    arr = np.zeros((max_y, max_x))
-    for y in range(max_y):
-        for x in range(max_x):
-            arr[y, x] = pnoise2(x / freq, y / freq, octaves, base=base)
-    plt.figure()
-    plt.imshow(arr)
-    plt.show()
-    print('RMSE: {:g}'.format(np.sqrt(np.mean(arr**2))))
+plt.ioff()
+for base in [0, 1, 2, 3, 4]:
+    for freq in [12, 8, 6, 4]:
+        arr = np.zeros((max_y, max_x))
+        for y in range(max_y):
+            for x in range(max_x):
+                arr[y, x] = pnoise2(x / freq, y / freq, octaves, base=base)
+        plt.figure()
+        plt.imshow(arr)
+        plt.title('base {:g} freq {:g}'.format(base, freq))
+        plt.show()
+        print('RMSE: {:g}'.format(np.sqrt(np.mean(arr**2))))
+
+plt.ion()
 
 print('end')