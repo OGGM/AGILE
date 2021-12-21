0bc5803ecd3a0efc78407d6a42ac1b29b1e1f6ac
Patrick <patrick.schmitt@uibk.ac.at>
2021-12-21 12:43:33 +0100

    added tests for special_gradient_functions

--- combine1d/tests/test_special_gradient_functions.py
+++ combine1d/tests/test_special_gradient_functions.py
@@ -1,4 +1,17 @@
+import torch
+from torch.autograd import gradcheck
+from combine1d.core.special_gradient_functions import para_width_from_thick,\
+    para_thick_from_section
 
 
 def test_special_gradient_functions():
-    raise NotImplementedError()
+    test_shape = torch.abs(torch.randn(1000, dtype=torch.double, requires_grad=True))
+    test_thick = torch.abs(torch.randn(1000, dtype=torch.double, requires_grad=True)) * 1000
+
+    input_shape_thick = (test_shape, test_thick)
+    assert gradcheck(para_width_from_thick.apply, input_shape_thick)
+
+    test_section = torch.abs(torch.randn(1000, dtype=torch.double, requires_grad=True)) * 100
+
+    input_shape_section = (test_shape, test_section)
+    assert gradcheck(para_thick_from_section.apply, input_shape_section)
