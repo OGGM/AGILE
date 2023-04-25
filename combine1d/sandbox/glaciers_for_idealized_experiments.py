8244dd7407f5eeccdf0a10cfe5b881cd7e45da76
Patrick <patrick.schmitt@uibk.ac.at>
2023-04-25 17:31:48 +0200

    adapted creation of synthetic glaciers

--- combine1d/sandbox/glaciers_for_idealized_experiments.py
+++ combine1d/sandbox/glaciers_for_idealized_experiments.py
@@ -1,31 +1,18 @@
-# Here are the glaciers defined which are used for the experiments,
-# t_bias_limits must be set by hand (set to None and use
-# create_idealized_experiment function), and also set t_bias_spinup to speed
-# up the creation of the experiments
+# Here are the glaciers defined which are used for the experiments
 experiment_glaciers = {
     'Baltoro': {
         'rgi_id': 'RGI60-14.06794',
-        't_bias_spinup_limits': (-4, -3),  # not used during creation
-        't_bias_spinup': -7,
     },
     'Hintereisferner': {
         'rgi_id': 'RGI60-11.00897',
-        't_bias_spinup_limits': (-1, 0),  # not used during creation
-        't_bias_spinup': 1.4,
     },
     'Aletsch': {
         'rgi_id': 'RGI60-11.01450',
-        't_bias_spinup_limits': (-1, 0),  # not used during creation
-        't_bias_spinup': 2.9,
     },
     'Artesonraju': {
         'rgi_id': 'RGI60-16.02444',
-        't_bias_spinup_limits': (-1, 0),  # not used during creation
-        't_bias_spinup': 5,
     },
     'Shallap': {
         'rgi_id': 'RGI60-16.02207',
-        't_bias_spinup_limits': (-2, -1),  # not used during creation
-        't_bias_spinup': 1,
     },
 }
