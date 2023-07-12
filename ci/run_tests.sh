27297a47509bb9a23c6322bc2606c8ae4d1b5d32
Patrick <patrick.schmitt@uibk.ac.at>
2023-07-12 10:48:09 +0200

    adapt github worflows to new name

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -18,6 +18,6 @@ coverage erase
 coverage run --source=./agile1d --parallel-mode --module \
     pytest --verbose --run-test-env $TEST_ENV agile1d
 
-coverage combine
+coverage agile
 coverage xml
 coverage report --skip-covered
